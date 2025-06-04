2e6738baae5b02640717dd56d6e0e2f3111ef759
Carlos Chirito <cchirito@aynitech.com>
2025-06-03 21:24:49 -0500

    feat: add application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,33 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { globalStyles } from '@/styles/global-styles';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import * as NavigationBar from 'expo-navigation-bar';
+import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Platform, View } from 'react-native';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const isAndroid = Platform.OS === 'android';
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+if(isAndroid) {
+  NavigationBar.setBackgroundColorAsync('black')
+}
+
+const RootLayout = () => {
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  })
 
   if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
+    return null // or a loading indicator
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={ globalStyles.background }>
+      <Slot />
+
+      <StatusBar style="light"/>
+    </View>
   );
 }
+
+export default RootLayout;
